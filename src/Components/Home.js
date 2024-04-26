91f1ae341d32e63e1a1f71847913bca52d12b649
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-04-26 10:51:30 +0530

    done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 
 const Home = () => {
   return (
-    <div className="md:w-[50%] w-[90%] flex flex-col items-center bg-[#b5dfff] h-full md:m-5 rounded-3xl border-black border-[5px]">
+    <div className="md:w-[50%] w-[90%] flex flex-col items-center justify-center bg-[#b5dfff] h-full md:m-5 rounded-3xl border-black border-[5px]">
       <Navbar/>
       <div className='flex flex-col gap-20 m-4 p-4 mt-16 items-center w-full'>
         <NavLink to="/video1">
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex justify-center items-center w-full text-center'>
         <h1 className=" md:text-6xl text-4xl font-bold m-3 p-3">Parking Spot Detector</h1>
     </div>
   )
